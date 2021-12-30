69a5cd978249ecd2df386a49115eae1652c6de49
”Navn <laur6819@stud.kea.dk>
2021-12-30 15:48:16 +0100

    burgermenu i javascript

---
+++ javascript.js
@@ -0,0 +1,17 @@
+//BURGERMENU//
+const knapper = document.querySelector("button");
+const nav = document.querySelector("nav");
+
+knapper.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  nav.classList.toggle("shown");
+
+  const menuShown = nav.classList.contains("shown");
+
+  if (menuShown) {
+    knapper.classList.add("open");
+  } else {
+    knapper.classList.remove("open");
+  }
+}
