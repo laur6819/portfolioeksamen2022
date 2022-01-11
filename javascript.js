af391c6555bef95ed5f07fee94b819d84b0f51e7
”Navn <laur6819@stud.kea.dk>
2022-01-11 18:41:31 +0100

    noter til javascript

--- javascript.js
+++ javascript.js
@@ -2,13 +2,14 @@
 const knapper = document.querySelector("button");
 const nav = document.querySelector("nav");
 
+//her lytter den efter et klik for at "toggle" og vise menuen
 knapper.addEventListener("click", toggleMenu);
-
+//her starter den funktionen hvor den viser navigationen der er gemt i siden
 function toggleMenu() {
   nav.classList.toggle("shown");
-
+  //
   const menuShown = nav.classList.contains("shown");
-
+  //hvis menuen er åben og man trykker på krydset
   if (menuShown) {
     knapper.classList.add("open");
   } else {
